4f5857ea1db4b0125a37a809e97640180b7df5cf
venubabu <velpurivenubabu123@gmail.com>
2023-04-17 17:32:06 +0530

    [Venubabu] Done With FabanocciSeries

---
+++ FunctionalAndLogicalProramming/FabanocciSeries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalAndLogicalProramming
+{
+    internal class FabanocciSeries
+    {
+        public static int getFebnocciSeries(int input)
+        {
+            
+            if (input == 0 || input == 1)
+            {
+                return input;
+            }
+            else
+                return getFebnocciSeries(input - 1) + getFebnocciSeries(input - 2);
+
+        }
+    }
+}
--- FunctionalAndLogicalProramming/Program.cs
+++ FunctionalAndLogicalProramming/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical Programming!");
+            Console.WriteLine("Enter Number Witch want to find Fabanocci Term");
+            int input=Convert.ToInt32(Console.ReadLine());
+            if (input < 0)
+                Console.WriteLine("Number should be 0 or Positive number");
+            else
+                Console.WriteLine(FabanocciSeries.getFebnocciSeries(input));
         }
     }
 }
