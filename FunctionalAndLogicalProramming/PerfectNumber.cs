d17023c808409b413be2f10f9abdd5122ba836f1
venubabu <velpurivenubabu123@gmail.com>
2023-04-17 18:34:23 +0530

    [Venubabu] Done With PerfectNumber

---
+++ FunctionalAndLogicalProramming/PerfectNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace FunctionalAndLogicalProramming
+{
+    internal class PerfectNumber
+    {
+        public static void PerfectNumberOrNot( int number) {
+            int total = 0;
+            for (int i = 1; i < number; i++)
+            {
+                
+                if (number%i==0)
+                {
+                    total += i;
+                }
+            }
+            if (total == number)
+                Console.WriteLine("It's a Perfect Number");
+            else
+                Console.WriteLine("It's Not a Perfect Number");
+
+        }
+    }
+}
--- FunctionalAndLogicalProramming/Program.cs
+++ FunctionalAndLogicalProramming/Program.cs
@@ -4,13 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to logical Programming!");
-            Console.WriteLine("Enter Number Witch want to find Fabanocci Term");
-            int input=Convert.ToInt32(Console.ReadLine());
-            if (input < 0)
-                Console.WriteLine("Number should be 0 or Positive number");
-            else
-                Console.WriteLine(FabanocciSeries.getFebnocciSeries(input));
+            Console.WriteLine("Enter bellow options");
+            Console.WriteLine("1.Fabanocci/n2.PerfectNumber");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            { 
+                case 1:
+
+                    Console.WriteLine("Welcome to logical Programming!");
+                    Console.WriteLine("Enter Number Witch want to find Fabanocci Term");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    if (input < 0)
+                        Console.WriteLine("Number should be 0 or Positive number");
+                    else
+                        Console.WriteLine(FabanocciSeries.getFebnocciSeries(input));
+                    break;
+                case 2:
+                    Console.WriteLine("Enter number witch you want to find perfect Number or not");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    PerfectNumber.PerfectNumberOrNot(number);
+
+                    break;
+
+            }
         }
     }
 }
