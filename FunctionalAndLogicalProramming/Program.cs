246f1a5bfe81d0ba00acd15838d6f53110795cf2
venubabu <velpurivenubabu123@gmail.com>
2023-04-17 16:06:56 +0530

    [Venubabu] Added Welcome Message

---
+++ FunctionalAndLogicalProramming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalAndLogicalProramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical Programming!");
+        }
+    }
+}
